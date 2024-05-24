717d56cabafa24ef17221f3413db1510180f7e42
sunsonni <sonniemnguyen@gmail.com>
2024-05-23 21:09:50 -0500

    still test in task two not passing but continuing

--- TechJobsOOAutoGraded6/Location.cs
+++ TechJobsOOAutoGraded6/Location.cs
@@ -14,15 +14,14 @@ namespace TechJobsOOAutoGraded6
         }
 
         // TODO: Task 2: Add a second constructor to this class that uses the Location() constructor and sets the value of the value field.
-        public Location(string value): this()
+        public Location(string value) : this()
         {
             Value = value;
         }
 
         public override bool Equals(object obj)
         {
-            return obj is Location location &&
-                   Id == location.Id;
+            return obj is Location location && Id == location.Id;
         }
 
         public override int GetHashCode()
